fb98cb9099be7eec2177aa04a7271cdd79611467
Raymond <theflyingraymond@gmail.com>
2024-11-30 11:37:22 +0800

    两数之和

---
+++ leetcode/100/LC01.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC01 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int a = target - nums[i];
+            if (map.containsKey(a)) {
+                return new int[]{i, map.get(a)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
