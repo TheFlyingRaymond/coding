81bd4e4f515292d0ec2e04139109478765b518f4
suncheng11 <suncheng11@meituan.com>
2024-12-01 16:23:25 +0800

    lc496加一个暴力法

--- leetcode/400/LC496.java
+++ leetcode/400/LC496.java
@@ -8,6 +8,7 @@ public class LC496 {
         int[] nums1 = {4,1,2};
         int[] nums2 = {1,3,4,2};
         System.out.println(Arrays.toString(new LC496().nextGreaterElement(nums1, nums2)));
+        System.out.println(Arrays.toString(new LC496().nextGreaterElement2(nums1, nums2)));
     }
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -32,5 +33,31 @@ public class LC496 {
         }
 
         return ret;
+    }
+
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+
+        int[] ret = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ret[i] = getNext(nums1[i], nums2, map);
+        }
+        return ret;
+    }
+
+    private int getNext(int num, int[] nums2, Map<Integer, Integer> map) {
+        int idx = map.get(num);
+
+        for(int i = idx + 1; i < nums2.length; i++) {
+            if (nums2[i] > num) {
+                return nums2[i];
+            }
+        }
+
+        return -1;
     }
 }
